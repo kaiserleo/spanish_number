26dd12a0c8bb17804e53fcbb25b0e061b1474b9b
Ruben Espinosa <rderoldan1@gmail.com>
2014-05-15 10:40:37 -0500

    travis

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@ require 'rubygems'
 require 'bundler/setup'
 require 'coveralls'
 
-Coveralls.wear! 'rails'
+Coveralls.wear!
 
 require File.expand_path('../array', __FILE__)
 require File.expand_path('../../lib/spanish_number', __FILE__)
